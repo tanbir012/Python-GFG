63e84c73dfc4246cc544f6f83d099a5916a46c2a
Tanbir Singh <163909963+tanbir012@users.noreply.github.com>
2025-07-31 01:21:03 -0400

    Update problems4.py

--- WEEK-1/DAY-1/Problems/problems4.py
+++ WEEK-1/DAY-1/Problems/problems4.py
@@ -1,1 +1,10 @@
+'''
+   Given two inputs that are stored in variables a and n, you need to print a, n times in a single line without space between them. Output must have a newline at the end.
 
+   '''
+a = input()
+n = int(input())
+#code here
+m = a * n
+
+print(m)
