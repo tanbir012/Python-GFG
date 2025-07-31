429fc0a9b27868e554d8f1e32ecb10363a48b9ca
Tanbir Singh <163909963+tanbir012@users.noreply.github.com>
2025-07-31 00:49:49 -0400

    Update problems1.py

--- WEEK-1/DAY-1/Problems/problems1.py
+++ WEEK-1/DAY-1/Problems/problems1.py
@@ -1,1 +1,5 @@
+# qns.1  When learning a new language, we first learn to output some message. Here we will start with the famous "Hello World" message.
+# Complete the python code to print "Hello World". 
 
+# Ans :-
+  print("Hello World")
