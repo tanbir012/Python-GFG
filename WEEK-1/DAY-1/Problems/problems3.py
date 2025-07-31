4cb6293a5f43655641d8eb65924054452f7d8c3c
Tanbir Singh <163909963+tanbir012@users.noreply.github.com>
2025-07-31 01:17:23 -0400

    Update problems3.py

--- WEEK-1/DAY-1/Problems/problems3.py
+++ WEEK-1/DAY-1/Problems/problems3.py
@@ -1,1 +1,14 @@
+'''
+You need to perform three separate tasks based on the given input:
 
+String Input and Print: Take a text input as a string and print it as it is.
+Integer Input and Add: Take an integer input n, add 10 to it, and print the result.
+Float Input and Multiply: Take a floating-point number as input, multiply it by 10, and print the result.
+'''
+s = input()
+n = int(input())
+f = float(input())
+
+print(s)
+print(n + 10)
+print(f * 10)
